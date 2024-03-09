70633035abd0234ec70ccffa75635168561fa375
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-03-09 10:42:46 +0000

    change debug back to dev in os.environ

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -60,10 +60,11 @@ REST_AUTH = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOSTS'),
+    '.gitpod.io',
    'localhost',
 ]
 
