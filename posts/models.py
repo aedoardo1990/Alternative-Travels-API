e04f84bb82aec27109d41e72502b681d8967a24d
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-03-28 17:41:43 +0000

    revert back models and serializers to only image field

--- posts/models.py
+++ posts/models.py
@@ -1,22 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from tagulous.models import TagField
-from cloudinary_storage.storage import VideoMediaCloudinaryStorage, MediaCloudinaryStorage
-from django.core.files.storage import FileSystemStorage
-
-
-class CustomStorage(FileSystemStorage):
-    def get_valid_name(self, name):
-        if name:
-            if name.endswith(('.jpg', '.jpeg', '.png', '.gif')):
-                # Images will be stored in Cloudinary using the MediaCloudinaryStorage
-                return 'images/' + name
-            elif name.endswith(('.mp4', '.mov', '.avi')):
-                # Videos will be stored in Cloudinary using the VideoMediaCloudinaryStorage
-                return 'videos/' + name
-        # If the file extension is unknown or the instance/filename is not available,
-        # use the default location and apply the default valid name logic
-        return super().get_valid_name(name)
 
 
 class Post(models.Model):
@@ -40,21 +24,12 @@ class Post(models.Model):
     tags = TagField(force_lowercase=True, max_count=15)
     latitude = models.FloatField()
     longitude = models.FloatField()
-    image = models.FileField(upload_to = 'files/', storage=CustomStorage())
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_g5kn5h', blank=True
+    )
     image_filter = models.CharField(
         max_length=32, choices=image_filter_choices, default='normal'
     )
-
-    def save(self, *args, **kwargs):
-        if self.test_upload:
-            if self.test_upload.name.endswith(('.jpg', '.jpeg', '.png', '.gif')):
-                # Use MediaCloudinaryStorage for images
-                self.test_upload.storage = MediaCloudinaryStorage()
-            elif self.test_upload.name.endswith(('.mp4', '.mov', '.avi')):
-                # Use VideoMediaCloudinaryStorage for videos
-                self.test_upload.storage = VideoMediaCloudinaryStorage()
-
-        super().save(*args, **kwargs)
 
     class Meta:
         ordering = ['-created_at']
