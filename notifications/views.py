113036d01c09618b738d64ad1161a4bf86fa14d3
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-03-29 17:24:00 +0000

    add views and urls to Notifications app

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Notification
+from .serializers import NotificationSerializer
 
-# Create your views here.
+
+class NotificationList(generics.ListCreateAPIView):
+    """
+    view to check all notifications
+    """ 
+    permissions_classes=[permissions.IsAuthenticatedOrReadOnly]
+    serializer_class=NotificationSerializer
+    queryset=Notification.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class NotificationDetail(generics.RetrieveDestroyAPIView):
+    """
+    view to retrieve a notification
+    """
+    permissions_classes = [IsOwnerOrReadOnly]
+    serializer_class = NotificationSerializer
+    queryset = Notification.objects.all()
