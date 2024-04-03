6b722df2ef3b033a6ecab7dc97e269720f5ddcdc
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-04-03 17:11:13 +0000

    create serializers for Model Marketplace

--- marketplace/models.py
+++ marketplace/models.py
@@ -8,6 +8,15 @@ class Marketplace(models.Model):
     """
     Model to post products to post items to sell on the Marketplace
     """
+    image_filter_choices = [
+    ('_1977', '1977'), ('brannan', 'Brannan'),
+    ('earlybird', 'Earlybird'), ('hudson', 'Hudson'),
+    ('inkwell', 'Inkwell'), ('lofi', 'Lo-Fi'),
+    ('kelvin', 'Kelvin'), ('normal', 'Normal'),
+    ('nashville', 'Nashville'), ('rise', 'Rise'),
+    ('toaster', 'Toaster'), ('valencia', 'Valencia'),
+    ('walden', 'Walden'), ('xpro2', 'X-pro II')
+    ]
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -24,6 +33,9 @@ class Marketplace(models.Model):
     address = models.CharField(max_length=255)
     contact_number = models.IntegerField()
     email = models.EmailField()
+    image_filter = models.CharField(
+        max_length=32, choices=image_filter_choices, default='normal'
+    )
 
     class Meta:
         ordering = ['-created_at']
