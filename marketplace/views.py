81e53711207c97412492f5d03a5a8d3e4de44190
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-04-03 17:19:13 +0000

    create view of Marketplace Model

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.db.models import Count
+from rest_framework import permissions, generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Marketplace
+from .serializers import MarketplaceSerializer, MarketplaceDetailSerializer
 
-# Create your views here.
+
+class MarketplaceList(generics.ListCreateAPIView):
+    """
+    View to list posts on Marketplace
+    """
+    serializer_class = MarketplaceSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+    queryset = Marketplace.objects.annotate(
+        comments_count = Count('comment', distinct=True),
+        likes_count = Count('likes', distinct=True),
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend
+    ]
+    ordering_fields = [
+        'comments_count',
+        'likes_count',
+        'likes__created_at'
+    ]
+    search_fields = [
+        'owner__username',
+        'title',
+        'condition',
+        'price',
+    ]
+    # to filter posts 1) user is following, 2) posts user liked, 3) posts owned by a user
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'likes__owner__profile',
+        'owner__profile'
+    ]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class MarketplaceDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    View to check details of a post on Marketplace
+    """
+    serializer_class = MarketplaceDetailSerializer
+    permission_classes = [ IsOwnerOrReadOnly]
+    queryset = Marketplace.objects.annotate(
+        comments_count = Count('comment', distinct=True),
+        likes_count = Count('likes', distinct=True),
+    ).order_by('-created_at')
