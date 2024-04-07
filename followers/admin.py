70ab75be97a0dc8fe90d23da1974ab053f321065
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-04-07 20:51:05 +0000

    update followe admin.py

--- followers/admin.py
+++ followers/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Follower
 
 
-admin.site.register(Follower)
+@admin.register(Follower)
+class FollowerAdmin(admin.ModelAdmin):
+    list_display = [
+        'owner',
+        'followed',
+        'created_at'
+        ]
